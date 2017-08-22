1bdab85a0e1322768ff2bc1e04bd9420d403e1fe
Tuoris <tuoris.dev@gmail.com>
2017-08-22 17:54:12 +0300

    Add initial package setup

---
+++ setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.0.1'
+
+requires = [
+    'setuptools',
+]
+
+test_requires = requires + [
+    'python-coveralls',
+]
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(here, 'README.md')) as f:
+    README = f.read()
+
+setup(name='esculator',
+      version=version,
+      description="",
+      long_description=README,
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python",
+        ],
+      keywords="",
+      author='Quintagroup, Ltd.',
+      author_email='info@quintagroup.com',
+      url='https://github.com/openprocurement/esculator',
+      license='Apache License 2.0',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['esculator'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=requires,
+      extras_require={'test': test_requires},
+      test_suite="esculator.tests.main.suite")
