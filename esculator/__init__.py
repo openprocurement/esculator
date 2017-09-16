b13cc0a3cade31de3955978fe14afc705f4d23b2
annawzz <annawzz@quintagroup.org>
2017-09-16 18:15:25 +0300

    Make dummy temporary functions for testing

--- esculator/__init__.py
+++ esculator/__init__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 #XXX TODO from npv_calculation import calculate_npv as npv
 def npv(*args, **kw):
-    return 1
+    return float(args[1][0])
 
 #XXX TODO from npv_calculation import calculate_escp as escp
 def escp(*args, **kw):
-    return 1
+    return float(args[1][0])
