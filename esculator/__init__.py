1d92a2c3ae0376f52bd799c1506f5e75343d3af8
Volodymyr Maksymiv <vmaksymiv@quintagroup.com>
2017-09-17 23:19:15 +0300

    import real definitions

--- esculator/__init__.py
+++ esculator/__init__.py
@@ -1,8 +1,2 @@
 # -*- coding: utf-8 -*-
-#XXX TODO from npv_calculation import calculate_npv as npv
-def npv(*args, **kw):
-    return float(args[1][0])
-
-#XXX TODO from npv_calculation import calculate_escp as escp
-def escp(*args, **kw):
-    return float(args[1][0])
+from esculator.calculations import npv, escp  # pylint: disable=W0611
